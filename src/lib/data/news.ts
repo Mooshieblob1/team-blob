84545f82e220b347151f601d510b62269bf9c636
Mooshieblob1 <blob@mooshieblob.com>
2025-05-01 21:19:41 +0800

    fix(news): correct spelling of "behaviour" in news article

--- src/lib/data/news.ts
+++ src/lib/data/news.ts
@@ -8,6 +8,6 @@ export const news = [
 		title: 'Blob Breaks Record for Yone Deaths',
 		date: '2025-04-28',
 		content:
-			'Despite dying 13 times, Blob maintained a positive attitude and a 71% winrate. King behavior.'
+			'Despite dying 13 times, Blob maintained a positive attitude and a 71% winrate. King behaviour.'
 	}
 ];
